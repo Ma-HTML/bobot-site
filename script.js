be10fb23ae35a99bfe8cbc9bdc11df810c7a4e8e
Max_HTML <155242315+Ma-HTML@users.noreply.github.com>
2024-06-16 00:56:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Animation de présentation
+  var logo = document.getElementById('logo');
+  var subtitle = document.getElementById('subtitle');
+  
+  logo.style.opacity = '0';
+  subtitle.style.opacity = '0';
+  
+  setTimeout(function() {
+    logo.style.transition = 'opacity 1s ease-in-out';
+    subtitle.style.transition = 'opacity 1s ease-in-out';
+    logo.style.opacity = '1';
+    subtitle.style.opacity = '1';
+  }, 500);
+  
+  // Redirection vers la page d'invitation Discord
+  var inviteBtn = document.getElementById('inviteBtn');
+  inviteBtn.addEventListener('click', function() {
+    window.location.href = 'invite.html'; // Rediriger vers la page d'invitation
+  });
+});
 
